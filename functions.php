ce0d1e409b46cbc330213adbd2568ea40c4b9c72
Raquel Ocasio <raquel@raquelocasio.com>
2022-04-11 21:49:35 -0400

    added menu support and created menus

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
 
 // Enqueue own scripts
 function enqueue_custom_scripts() {
+    wp_deregister_script('jquery');
     wp_enqueue_script( 'jquery', get_template_directory_uri() .'/assets/vendors/jquery/jquery-3.4.1.js', array(),true);
     wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() .'/assets/vendors/bootstrap/bootstrap.bundle.js', array(),true);
     wp_enqueue_script( 'bootstrapaffixjs', get_template_directory_uri() .'/assets/vendors/bootstrap/bootstrap.affix.js', array(),true);
@@ -19,5 +20,18 @@ function enqueue_custom_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
                 
-                
+// Add menus
+add_theme_support('menus');
+
+function register_nav_menus() {
+
+    $locations = array(
+           'top-menu' => __( 'Top Menu', 'theme' ),
+           'bottom-menu' => __( 'Bottom Menu', 'theme' ),
+        
+    );
+    register_nav_menus( $locations );
+
+}
+add_action( 'init', 'register_nav_menus' );
 ?>
