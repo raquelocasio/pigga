13cc9f35ba99b89df55e9c1ae10b6c49b494a202
Raquel Ocasio <raquel@raquelocasio.com>
2022-04-11 21:12:23 -0400

    enqueued scripts

--- functions.php
+++ functions.php
@@ -7,5 +7,17 @@ function enqueue_custom_styles() {
                       
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
+
+
+// Enqueue own scripts
+function enqueue_custom_scripts() {
+    wp_enqueue_script( 'jquery', get_template_directory_uri() .'/assets/vendors/jquery/jquery-3.4.1.js', array(),true);
+    wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() .'/assets/vendors/bootstrap/bootstrap.bundle.js', array(),true);
+    wp_enqueue_script( 'bootstrapaffixjs', get_template_directory_uri() .'/assets/vendors/bootstrap/bootstrap.affix.js', array(),true);
+    wp_enqueue_script( 'devcrudjs', get_template_directory_uri() .'/assets/js/pigga.js', array(),true);
+                      
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
+                
                 
 ?>
